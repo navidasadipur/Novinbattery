9561dd2e68c12e662faa754689765f3e0241b3c3
User <User@DESKTOP-5IFU6NT>
2021-07-17 10:58:12 +0430

    modify ProductDetailsViewModel

--- SpadStorePanel.Web/ViewModels/ProductViewModels.cs
+++ SpadStorePanel.Web/ViewModels/ProductViewModels.cs
@@ -140,17 +140,61 @@ namespace SpadStorePanel.Web.ViewModels
         public string Message { get; set; }
         public string AddedDate { get; set; }
     }
+
     public class ProductDetailsViewModel
     {
+        public ProductDetailsViewModel()
+        {
+            ProductGalleries = new List<ProductGallery>();
+            ProductMainFeatures = new List<ProductMainFeature>();
+            ProductFeatureValues = new List<ProductFeatureValue>();
+            ProductComments = new List<ProductCommentViewModel>();
+        }
+        public long Price { get; set; }
+        public long PriceAfterDiscount { get; set; }
+
         public Product Product { get; set; }
+        public CommentFormViewModel CommentForm { get; set; }
+        public ProductGroup Group { get; set; }
+
         public List<ProductGallery> ProductGalleries { get; set; }
         public List<ProductMainFeature> ProductMainFeatures { get; set; }
         public List<ProductFeatureValue> ProductFeatureValues { get; set; }
-        public long Price { get; set; }
-        public long PriceAfterDiscount { get; set; }
         public List<ProductCommentViewModel> ProductComments { get; set; }
-        public CommentFormViewModel CommentForm { get; set; }
-        public ProductGroup Group { get; set; }
+    }
+
+    //public class ProductDetailViewModel
+    //{
+    //    public Product Product { get; set; }
+    //    public List<ProductGallery> ProductGalleries { get; set; }
+    //    public ProductMainFeature ProductMainFeatures { get; set; }
+    //    public List<ProductFeatureValue> ProductFeatureValues { get; set; }
+    //    public List<ProductComment> Comments { get; set; }
+    //    public long Price { get; set; }
+    //    public long PriceAfterDiscount { get; set; }
+    //    public int DiscountPercentage { get; set; }
+
+    //}
+
+    public class ProductCommentFormViewModel
+    {
+        public int? ParentId { get; set; }
+        public int? ArticleId { get; set; }
+        public int? ProductId { get; set; }
+        [Display(Name = "نام")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(300, ErrorMessage = "{0} باید کمتر از 300 کارکتر باشد")]
+        public string Name { get; set; }
+        [Display(Name = "ایمیل")]
+        [EmailAddress(ErrorMessage = "ایمیل نا معتبر")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(400, ErrorMessage = "{0} باید کمتر از 400 کارکتر باشد")]
+        public string Email { get; set; }
+        [Display(Name = "پیام")]
+        [DataType(DataType.MultilineText)]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(800, ErrorMessage = "{0} باید کمتر از 800 کارکتر باشد")]
+        public string Message { get; set; }
     }
 
     public class DiscountProductViewModel
